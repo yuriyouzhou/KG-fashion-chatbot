a482fb46b67e5af72cc382d10fc505ab50df77d2
Zhou You <yollyhideout@gmail.com>
2018-04-01 14:30:34 +0800

    able to receive image and store it

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from get_sentiment_score import get_sentiment_score
 from get_topic import get_topic
 import json
+import os
 
 app = Flask(__name__)
+photos = UploadSet('photos', IMAGES)
+app.config['UPLOADED_PHOTOS_DEST'] = 'static/img'
+configure_uploads(app, photos)
 
 english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
 
@@ -26,6 +31,14 @@ def get_bot_response():
     topic = get_topic(userText)
 
     return json.dumps({'answer': answer, 'score':score, 'topic': topic})
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['photo']
+        file_name = photos.save(file)
+        return file_name
 
 
 if __name__ == "__main__":
