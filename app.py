fa14381984adb6d4c1d93b1b8983902270644bb0
Zhou You <yollyhideout@gmail.com>
2018-04-01 13:39:05 +0800

    add image upload in UI

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
 
-english_bot.set_trainer(ChatterBotCorpusTrainer)
-english_bot.train("chatterbot.corpus.english")
+# english_bot.set_trainer(ChatterBotCorpusTrainer)
+# english_bot.train("chatterbot.corpus.english")
 
 
 @app.route("/")
