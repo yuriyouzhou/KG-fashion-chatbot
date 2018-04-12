142665eebdb2ab6e99162043567fa32f3bbf6908
sirumissu <36250283+sirumissu@users.noreply.github.com>
2018-04-12 12:36:34 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,3 +1,33 @@
-from sklearn.externals import joblib
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 12 11:44:58 2018
 
-##load models and get prediction results
+@author: Siru
+"""
+import os
+path = 'E:\\1_1social_media_computing\\cs4242-mini-project-master'
+os.chdir(path)
+import numpy as np
+from keras.models import load_model
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+def get_response(input_text):
+    model = load_model('intent_lstm.pkl')
+    tk = Tokenizer()
+    tk.fit_on_texts(input_text)
+    index_list = tk.texts_to_sequences(input_text)
+    x_train = pad_sequences(index_list, maxlen=64)
+    predict = model.predict(x_train)
+    predict = np.argmax(np.bincount(predict.argmax(axis=-1)))
+    return predict
+
+def get_intent(input_text):
+    model = load_model('response_lstm.pkl')
+    tk = Tokenizer()
+    tk.fit_on_texts(input_text)
+    index_list = tk.texts_to_sequences(input_text)
+    x_train = pad_sequences(index_list, maxlen=64)
+    predict = model.predict(x_train)
+    predict = np.argmax(np.bincount(predict.argmax(axis=-1)))
+    return predict
