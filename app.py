76d84aa046b4b4611525c17380a111968c647050
Zhou You <yollyhideout@gmail.com>
2018-04-14 21:20:24 +0800

    support basic demo

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_uploads import UploadSet, configure_uploads, IMAGES
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 import json
-import os
+from os import path
 
 app = Flask(__name__)
 photos = UploadSet('photos', IMAGES)
@@ -15,46 +15,85 @@ english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStora
 # english_bot.set_trainer(ChatterBotCorpusTrainer)
 # english_bot.train("chatterbot.corpus.english")
 
-
 @app.route("/")
 def home():
+    global index
+    index = 0
     return render_template("chat.html")
 
 @app.route("/get")
 def get_bot_response():
-    userText = request.args.get('messageText')
+    msg = request.args.get('messageText')
+    if "hi" in msg or "hello" in msg:
+        clear_history()
 
-    answer = str(english_bot.get_response(userText))
-    # score = 0.0
-    # topic = 'shopping'
+    curr_turn = {
+        "type": "greeting",
+        "speaker": "user",
+        "utterence": {
+            "images": None,
+            "false nlg": None,
+            "nlg": msg
+        }
+    }
 
-    wordCount = len(answer.split(" "))
+    end_response =  {
+        "type": "greeting",
+        "speaker": "system",
+        "utterance": {
+            "images": None,
+            "false nlg": None,
+            "nlg": "This conversation is over  :)"
+        }
+    }
 
-    # return json.dumps({'answer': answer, 'score':score, 'topic': topic})
-    return json.dumps( {
-         "sentiment": {
-          "label": ["anticipation", "Joy"],
-          "anticipation": 0.941,
-          "joy": 0.73,
-          "trust": 0.59,
-          "fear": 0.01,
-          "surprise":0.042,
-          "sadness":0.05,
-          "disgust":0.03,
-          "anger": 0.01,
-         },
-         "wordCount": wordCount
-        })
+
+    with open('./history/curr_history.txt') as data_file:
+        old_data = json.load(data_file)
+        if old_data is None:
+            data = [curr_turn]
+            index = 1
+        else:
+            data = old_data
+            data.append(curr_turn)
+            index = len(data)
+            print("length of history", index)
+
+    with open('./history/1.json') as answer:
+        history = json.load(answer)
+        try:
+            if "user" in history[index]["speaker"]:
+                index = index + 1
+
+            response = [history[index ]]
+
+            if index < len(history) and "system" in history[index + 1]["speaker"]:
+                response.append(history[index + 1])
+                print(index+1)
+        except:
+            return json.dumps([end_response])
+
+        data.append(response)
+
+        with open('./history/curr_history.txt', 'w') as outfile:
+            outfile.write(json.dumps(data, outfile))
+
+        # print(response)
+        return json.dumps(response)
+
+
 
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
         file = request.files['photo']
-        # file_name = photos.save(file)
         return json.dumps({'answer': "%s has received!"%file.name})
 
 
+def clear_history():
+    with open("./history/curr_history.txt", 'w') as output:
+        output.write("null")
 
 if __name__ == "__main__":
     app.run()
